150c78436e88ee24fa454a764a022b46afa2335f
Cochonou26 <106807194+Cochonou26@users.noreply.github.com>
2022-11-27 23:12:12 +0100

    Add files via upload

---
+++ tests/fichierTest.s
@@ -0,0 +1,24 @@
+ADD $20, $5, $8
+ADDI $5, $9, 0
+AND $3, $8, $1
+BEQ $2, $9, 69
+BGTZ $31, 100
+BLEZ $0, 0
+BNE $5, $9, 11
+DIV $8, $3
+J $122
+JAL 365
+JR $22
+LUI $6, 39
+MFHI $0
+MFLO $10
+MULT $0, $2
+NOP
+OR $1, $2, $3
+ROTR $2, $9, $3
+SLL $8, $25, $3
+SLT $9, $0, 0
+SRL $0, $1, $9
+SUB $10, $0, $10
+SYSCALL
+XOR $1, $1, $1
